e290830ec66ffda80b535788b87be872ba827419
liushufan1104 <aeeoprsd110414@gmail.com>
2019-11-05 22:44:32 +0800

    Initial commit

---
+++ lib/src/main/java/com/example/lib/MyClass.java
@@ -0,0 +1,18 @@
+package com.example.lib;
+import java.util.Scanner;
+public class MyClass {
+    public static void main ( String[] args ) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter number");
+        int n = scanner.nextInt();
+        for(int i=n;i>0;i--){
+            for(int k=n-i ;k>0;k--){
+                System.out.print(" ");
+            }
+            for (int j=i;j>0;j--){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
